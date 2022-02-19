a4db17746a5c61f8fe56b963769ad7a95101ccf9
vsoch <vsoch@users.noreply.github.com>
2022-02-19 16:48:16 -0700

    test adding proposal again
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/clone.sh
+++ .github/scripts/clone.sh
@@ -6,9 +6,8 @@ BRANCH_FROM=${BRANCH_FROM:-gh-pages}
 printf "GitHub Actor: ${GITHUB_ACTOR}\n"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git branch
-git fetch
+git fetch --unshallow origin
 git checkout -b "${BRANCH_FROM}" || git checkout "${BRANCH_FROM}"
-git fetch --unshallow gh-pages
 git branch
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
