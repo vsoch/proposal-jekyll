d3ec9632f236a396da54e37ebfd206544c128919
vsoch <vsoch@users.noreply.github.com>
2022-02-19 16:37:52 -0700

    debug copy
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/create_pages.sh
+++ .github/scripts/create_pages.sh
@@ -5,6 +5,12 @@ set -e
 # Copy current site
 cp -R . /tmp/repo
 
+printf "Before checkout:\n"
+ls
+
+printf "Copied over:\n"
+ls /tmp/repo
+
 BRANCH_FROM=${BRANCH_FROM:-gh-pages}
 printf "GitHub Actor: ${GITHUB_ACTOR}\n"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
@@ -14,19 +20,28 @@ git branch
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
 
+printf "After checkout:\n"
+ls
+
 # We haven't created the site yet
 if  [[ ! -d "docs" ]]; then
     mv /tmp/repo/docs docs/
+    mv /tmp/repo/proposals proposals/
     mv /tmp/repo/.github .github
     mv /tmp/repo/README.md README.md
 else
     # Update current content to what is in main
     cp -R /tmp/repo/docs/* docs/
+    cp -R /tmp/repo/proposals/* proposals/
     cp -R /tmp/repo/.github/* .github/
     cp -R /tmp/repo/README.md README.md
 fi
 
-git add .
+git add README.md
+git add .github/*
+git add docs/*
+git add proposals/*
+
 if git diff-index --quiet HEAD --; then
     printf "No changes\n"
 else
