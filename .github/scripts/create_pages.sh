8723a27e00814dd84ae5ff447240b5092f224385
vsoch <vsoch@users.noreply.github.com>
2022-02-19 17:31:15 -0700

    try adding fetch
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/create_pages.sh
+++ .github/scripts/create_pages.sh
@@ -15,6 +15,7 @@ BRANCH_FROM=${BRANCH_FROM:-gh-pages}
 printf "GitHub Actor: ${GITHUB_ACTOR}\n"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git branch
+git fetch --unshallow origin
 git checkout -b "${BRANCH_FROM}" || git checkout "${BRANCH_FROM}"
 git branch
 git config --global user.name "github-actions"
